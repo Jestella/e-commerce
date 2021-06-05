1efe1fe1cecff40d680d8542757c656467907487
jestella <jestellaa@gmail.com>
2021-06-04 23:27:22 -0400

    add signup component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { Switch, Route } from "react-router-dom";
-
+import React from "react";
 import "./App.css";
-
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/signin-signup/signin-signup.component";
-import { auth } from "./firebase/firebase.utils";
-import React from "react";
-import { render } from "@testing-library/react";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -22,10 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
