1ab5faac82cf0df01cb82680a312d663e9bda55e
jestella <jestellaa@gmail.com>
2021-06-22 15:41:38 -0400

    change path

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,8 +6,15 @@ import CollectionPage from "../Collection-page/collection-page.component";
 
 const Shop = ({ match }) => (
   <div className="shop_page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    <Route
+      exact
+      path={`/e-commerce/${match.path}`}
+      component={CollectionsOverview}
+    />
+    <Route
+      path={`/e-commerce/${match.path}/:collectionId`}
+      component={CollectionPage}
+    />
   </div>
 );
 
